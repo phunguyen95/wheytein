bec99428d9b2e5efedfc57d7c2501d0c9329840c
Phu Nguyen <phu.nguyen09995@gmail.com>
2018-05-27 17:16:43 +1200

    remove trash files

--- client/public/img/Māori Dictionary_files/bRc.js
+++
@@ -1,9 +0,0 @@
-// Qualaroo for maoridictionary.co.nz
-// (C) 2015 Qualaroo. All rights reserved.
-// qualaroo.com
-
-//$ site: 48460, generated: 2015-11-30 17:11:46 UTC
-//$ type: base, rev. e7869d56 (deploy)
-//$ client: 2.0.14
-
-//Your Qualaroo account is suspended or disabled.  Please see your dashboard at https://app.qualaroo.com (or email support@qualaroo.com).
